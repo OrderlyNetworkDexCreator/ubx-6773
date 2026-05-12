49433430eefac2332b0e556892552df15da6e535
Mario Reder <mario.reder@pm.me>
2026-05-12 15:48:03 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -1,10 +1,11 @@
 window.__RUNTIME_CONFIG__ = {
   "VITE_ORDERLY_BROKER_ID": "demo",
   "VITE_ORDERLY_BROKER_NAME": "Ubc",
+  "VITE_BROKER_EOA_ADDRESS": "0x47a850aab6d9600e8910ea4d6c3c216c6650a09a",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
   "VITE_ORDERLY_MAINNET_CHAINS": "1,56,900900900,42161,10,8453",
-  "VITE_ORDERLY_TESTNET_CHAINS": "421614,84532,97,901901901,11124",
+  "VITE_ORDERLY_TESTNET_CHAINS": "421614,97,901901901,11124",
   "VITE_DEFAULT_CHAIN": "1",
   "VITE_PRIVY_APP_ID": "cmgh7ytpt003ijv0d68z3380z",
   "VITE_PRIVY_LOGIN_METHODS": "email,passkey,google",
@@ -17,8 +18,9 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "UBC is a fast, secure, and low-fee decentralized exchange where users trade crypto directly, without middlemen.",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Portfolio,Markets,Swap,Trading",
+  "VITE_ENABLED_MENUS": "Portfolio,Markets,Swap,Trading,Campaigns",
   "VITE_CUSTOM_MENUS": "",
+  "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "true",
   "VITE_TELEGRAM_URL": "https://t.me/ubcovenant",
   "VITE_DISCORD_URL": "",
@@ -34,5 +36,9 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_AVAILABLE_LANGUAGES": "en,zh,ja,es,ko,vi,de,fr,ru,id,tr,it,pt,uk,pl,nl",
   "VITE_USE_CUSTOM_PNL_POSTERS": "true",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "1",
-  "VITE_TRADING_VIEW_COLOR_CONFIG": ""
+  "VITE_TRADING_VIEW_COLOR_CONFIG": "",
+  "VITE_ANALYTICS_SCRIPT": "",
+  "VITE_SYMBOL_LIST": "",
+  "VITE_RESTRICTED_REGIONS": "",
+  "VITE_WHITELISTED_IPS": ""
 };
